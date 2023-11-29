c28ac6de685a7d12f0a701baf6d32e635ad8458b
Sai Teja <saitejapubglite@gmail.com>
2023-11-29 10:43:13 +0530

    measseg

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
 // Iteration 1: Make the Play button functional.
+const play = document.getElementById("play-button");
+play.onclick = () => {
+    window.location.href = "./game.html";
+};
+
 // Description: When the Play button is clicked the game page should be displayed.
--- score.js
+++ score.js
@@ -1,1 +1,11 @@
 // Iteration 5: Store the player score and display it on the game over screen
+let score = localStorage.getItem("score")
+console.log(score)
+
+var scoreBoard = document.getElementById("scoreBoard")
+var playAgainButton = document.getElementById("play-again-button")
+
+scoreBoard.innerHTML = score;
+playAgainButton.onclick = (Event) =>{
+    location.href = "./game.html"
+}
